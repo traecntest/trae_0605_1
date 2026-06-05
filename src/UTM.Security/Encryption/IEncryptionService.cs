ee65ece5f1349946f43f7038d0e1643bdf5e4cc0
co63oc <4617245+co63oc@users.noreply.github.com>
2026-06-05 21:41:56 +0800

    Fix

--- src/UTM.Security/Encryption/IEncryptionService.cs
+++
@@ -1,9 +0,0 @@
-namespace UTM.Security.Encryption;
-
-public interface IEncryptionService
-{
-    string Encrypt(string plainText, string key);
-    string Decrypt(string cipherText, string key);
-    byte[] EncryptBytes(byte[] plainData, byte[] key, byte[] iv);
-    byte[] DecryptBytes(byte[] cipherData, byte[] key, byte[] iv);
-}
